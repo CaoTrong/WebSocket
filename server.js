84e9c4425eb841e8c0df9dc402a9a42e3b48c2f0
CaoTrong <88994616+CaoTrong@users.noreply.github.com>
2025-05-07 16:41:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const PORT = process.env.PORT || 8080;
 const wss = new WebSocket.Server({ port: PORT });
 
 wss.on('connection', ws => {
-    ws.on('message', message => {
+   ws.on('message', message => {
+        // Giả sử message là chuỗi thuần, bọc vào JSON
+        const payload = JSON.stringify({ message });
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
+                client.send(payload);
             }
         });
     });
